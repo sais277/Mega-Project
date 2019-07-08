1c249206005fb53e8c6ffdbc0e99fda7485af0aa
sais277 <36911431+sais277@users.noreply.github.com>
2019-07-08 20:08:54 +0530

    Update Challenge-1.py

--- Challenge-1.py
+++ Challenge-1.py
@@ -8,7 +8,7 @@ create a program that will ask the users name, age, and reddit username. have it
 your name is (blank), you are (blank) years old, and your username is (blank)
 for extra credit, have the program log this information in a file to be accessed later.
 
-Solution:-
+Python Solution:-
 '''
 
 name=input("Enter name:")
